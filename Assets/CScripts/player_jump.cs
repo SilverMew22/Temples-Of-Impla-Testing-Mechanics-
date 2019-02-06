7aac529feca4fb6af4d6b9d0b9848da445fdb8f5
Courtney Ramos <courtneyramos65@gmail.com>
2019-02-06 16:34:02 -0500

    First Testing
    This is the first testing of code for my game, containing a jump and walk.

---
+++ Assets/CScripts/player_jump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_jump : MonoBehaviour {
+    public bool onGround;
+    private Rigidbody rb;
+ 
+    void Start() {
+        onGround = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+  
+    void Update()
+    {
+        if (onGround)
+        { if (Input.GetButtonDown("Jump"))
+            { rb.velocity = new Vector3(0f, 5f, 0f);
+                onGround = false; } } } 
+    
+	void OnCollisionEnter(Collision other)
+    { if (other.gameObject.CompareTag
+    ("Ground")){onGround = true;} } }
